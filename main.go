68ada364d0e0881eb57a9e1857d4514ffe18bc0c
mac <hendriarga28@gmail.com>
2024-11-24 06:46:40 +0700

    First Response Json

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"message": "Hello, World!"}`)
+}
+
+func bookIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
+	books := []map[string]string{
+		{"id": "1", "title": "Book 1"},
+		{"id": "2", "title": "Book 2"},
+		{"id": "3", "title": "Book 3"},
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
 func main() {
-	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, `{"message": "Hello, World!"}`)
-	})
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", getIndex).Methods("GET")
+
+	bookRouter := r.PathPrefix("/books").Subrouter()
+	bookRouter.HandleFunc("", bookIndex).Methods("GET")
 
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
